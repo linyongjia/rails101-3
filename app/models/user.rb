fa3738c3d15485f168501be0f48af8c920948b7b
yongjia <linyongjia0@live.cn>
2017-04-19 13:51:11 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-    has_many :gorups
-    has_many :posts
+  has_many :gorups
+  has_many :posts
 
-    has_many :group_relationships
-    has_many :patricipated_group, :through => :gtoup_relationships, :source => :group
-    
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationships, :source => :group
+
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
+
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+
 end
